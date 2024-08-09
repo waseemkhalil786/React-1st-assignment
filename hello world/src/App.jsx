1abbd7a3f907d4fd2a02421d6e7a872efbf5d8ac
Waseem khalil <waseemkhalilworl@gmail.com>
2024-08-09 01:06:43 -0700

    React 1st assignment done

---
+++ hello world/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+  return (
+   <h1>My Introduction !<span> <p className="primary">hi there my Name is Waseem Khalil .. <br /><br />I am a frontend developer .. <br /><br />I will pactice with ReactJs</p></span></h1>
+   
+  )
+}
+
+export default App
